fc029d535d6b1aa9ab709777bf7f820fbd5c5ecc
tomaszkluge <tomasz.kluge@gmail.com>
2024-02-25 21:38:54 +0100

    added check_resources functions

--- check_resources.py
+++ check_resources.py
@@ -1,13 +1,38 @@
 from data import coffees_resources
+from coins import insert_coin_espresso, insert_coin_latte, insert_coin_cappuccino
 
 
 def check_resources_espresso():
     espresso_resources = coffees_resources["espresso"]
 
-    for resource, amount in espresso_resources.item():
+    for resource, amount in espresso_resources.items():
         if resource != "money":
             if coffees_resources["resources"][resource] < amount:
                 print(f"Not enough {resource} to make espresso")
                 return False
+    return insert_coin_espresso()
 
-    return True
+
+def check_resources_latte():
+    latte_resources = coffees_resources["latte"]
+
+    for resource, amount in latte_resources.items():
+        if resource != "money":
+            if coffees_resources["resources"][resource] < amount:
+                print(f"Not enough {resource} to make latte")
+                return False
+    return insert_coin_latte()
+
+
+def check_resources_cappuccino():
+    cappuccino_resources = coffees_resources["cappuccino"]
+
+    for resource, amount in cappuccino_resources.items():
+        if resource != "money":
+            if coffees_resources["resources"][resource] < amount:
+                print(f"Not enough {resource} to make cappuccino")
+                return False
+    return insert_coin_cappuccino()
+
+
+
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from data import coffees_resources
-from coins import insert_coin_espresso, insert_coin_latte, insert_coin_cappuccino
+from check_resources import check_resources_espresso, check_resources_latte, check_resources_cappuccino
 
 
 def what_coffee():
     should_continue = True
     while should_continue:
-        ask = input("What would you like to drink? (espresso, latte or cappuccino): ")
+        ask = input("What would you like to drink? (espresso, latte or cappuccino): ").lower()
         if ask == "report":
             print(coffees_resources["resources"])
         elif ask == "off":
             print("Good Bye!")
             should_continue = False
         elif ask == "espresso":
-            insert_coin_espresso()
+            check_resources_espresso()
         elif ask == "latte":
-            insert_coin_latte()
+            check_resources_latte()
         elif ask == "cappuccino":
-            insert_coin_cappuccino()
+            check_resources_cappuccino()
 
         print(f"Rest of resources in the machine: {coffees_resources['resources']}")
 
